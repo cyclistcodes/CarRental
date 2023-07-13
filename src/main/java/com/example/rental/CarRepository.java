6190cc8633930d6add589bd3fbc88175b2b88dc3
Isaac Skog <isaacskog@MacBook-Air-som-tillhor-Isaac.local>
2023-07-13 19:19:46 +0200

    Add query getcarbyid

--- src/main/java/com/example/rental/CarRepository.java
+++ src/main/java/com/example/rental/CarRepository.java
@@ -2,6 +2,7 @@ package com.example.rental;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,8 @@ import java.util.List;
 @Repository
 public interface CarRepository extends JpaRepository<Car, Integer> {
     @Query(value = "SELECT * FROM car", nativeQuery = true)
-    List<Car> getAllCars();
+    public List<Car> getAllCars();
 
+    @Query(value = "SELECT * FROM car WHERE id = :id", nativeQuery = true)
+    public Car getCarById(@Param("id") int id);
 }
