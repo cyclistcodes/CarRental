523382a8482754babd7ea1d3c5a6f23b28d5fb6c
Isaac Skog <isaacskog@MacBook-Air-som-tillhor-Isaac.local>
2023-07-13 17:13:04 +0200

    Add repository: Rental

---
+++ src/main/java/com/example/rental/RentalRepository.java
@@ -0,0 +1,11 @@
+package com.example.rental;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface RentalRepository extends JpaRepository<Rental, Integer> {
+	@Query(value = "SELECT * FROM Rental", nativeQuery = true)
+	List<Car> getAllRentals();
+}
