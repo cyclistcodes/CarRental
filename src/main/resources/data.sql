ff73efa3a81b03617498b6dd08c19aa558d7da76
Isaac Skog <isaacskog@MacBook-Air-som-tillhor-Isaac.local>
2023-07-13 16:41:59 +0200

    Add initial Car data to database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO car (model, price) VALUES ('Volvo S60', 1500);
+INSERT INTO car (model, price) VALUES ('Volkswagen Golf', 1333);
+INSERT INTO car (model, price) VALUES ('Ford Mustang', 3000);
+INSERT INTO car (model, price) VALUES ('Ford Transit', 2400);
