cd7c187d19ba4fc0ddc41a9d4d970e5f4615658b
Isaac Skog <isaacskog@MacBook-Air-som-tillhor-Isaac.local>
2023-07-13 19:20:18 +0200

    Rename application

--- src/main/java/com/example/rental/RentalCarApplication.java
+++ src/main/java/com/example/rental/RentalCarApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RentalApplication {
+public class RentalCarApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RentalApplication.class, args);
+		SpringApplication.run(RentalCarApplication.class, args);
 	}
 
 }
