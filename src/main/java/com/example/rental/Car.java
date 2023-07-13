40f3fa4e697b04fa3c0f47bfb15553c366070f93
Isaac Skog <isaacskog@MacBook-Air-som-tillhor-Isaac.local>
2023-07-13 15:45:57 +0200

    Add class: Car

---
+++ src/main/java/com/example/rental/Car.java
@@ -0,0 +1,38 @@
+package com.example.rental;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String model;
+    @Column(nullable = false)
+    private Integer price;
+
+    public Car() {
+    }
+
+    public Car(String model, Integer price) {
+        this.model = model;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Integer getPrice() {
+        return price;
+    } 
+}
