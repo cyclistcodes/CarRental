cf08d7260f6cf10de4e58b9f15e12b1773d436a8
Isaac Skog <isaacskog@airsomthorisaac.home>
2023-07-13 20:06:27 +0200

    Add controller and get/post

--- src/main/java/com/example/rental/RentalCarController.java
+++ src/main/java/com/example/rental/RentalCarController.java
@@ -1,19 +1,27 @@
 package com.example.rental;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/")
-public class RentalController {
-
+public class RentalCarController {
+	private final RentalCarService service;
 	private final RentalRepository rentalRepository;
-
-	public RentalController(RentalRepository rentalRepository) {
+	private final CarRepository carRepository;
+	@Autowired
+	public RentalCarController(RentalCarService service, RentalRepository rentalRepository, CarRepository carRepository) {
 		this.rentalRepository = rentalRepository;
+		this.carRepository=carRepository;
+		this.service=service;
 	}
-	/*@GetMapping
-	public HelloWorld sayHello() {
-		return helloWorldRepository.getHelloWorldFromDatabase();
-	}*/
+	@GetMapping("/admin")
+	public List<Rental> showAdmin() {
+		return service.getAllRentals();
+	}
+	@PostMapping("/booking")
+	public Rental bookCar(@RequestBody Rental rental) {
+		return service.saveRental(rental);
+	}
 }
