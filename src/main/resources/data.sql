72190a37a14b087153b18fae98944ed9c5be0f62
Isaac Skog <isaacskog@98:46:0a:92:eb:a8.home>
2023-07-14 08:29:31 +0200

    Add testdata for Rental table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,7 @@ INSERT INTO car (model, price) VALUES ('Volvo S60', 1500);
 INSERT INTO car (model, price) VALUES ('Volkswagen Golf', 1333);
 INSERT INTO car (model, price) VALUES ('Ford Mustang', 3000);
 INSERT INTO car (model, price) VALUES ('Ford Transit', 2400);
+INSERT INTO rental (full_name, birth_date, car_model, start_date, end_date, total_revenue)
+VALUES ('John Doe', '1980-01-15', 'Ford Transit', '2022-07-01', '2022-07-05', 1400);
+INSERT INTO rental (full_name, birth_date, car_model, start_date, end_date, total_revenue)
+VALUES ('Jane Smith', '1975-09-22', 'Ford Mustang', '2022-07-03', '2022-07-08', 1700);
